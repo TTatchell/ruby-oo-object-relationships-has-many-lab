1526a92cbd3a231aa91933c99bc719676f28fbc3
TTatchell <tim.tatchell24@gmail.com>
2021-07-09 14:35:55 +1000

    finished

---
+++ lib/post.rb
@@ -0,0 +1,32 @@
+class Post
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def author_name
+    if self.author.to_s.strip.empty?
+      return nil
+    else
+      self.author.name
+    end
+  end
+end
